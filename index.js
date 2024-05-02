7d2d19de312ab32b051ad2ea8f9fd413ecf1cd7e
Ujjwal Sharma <ryzokuken@disroot.org>
2024-05-02 19:48:27 +0200

    add everything

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-const { MessageFormat } = require("messageformat")
-
-const msg =
-    "Click <link>here</link>. {{count}} or <b>{{count}}</b>. <icon/> is an icon."
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var messageformat_1 = require("messageformat");
+var msg = "Click <link>here</link>. {{count}} or <b>{{count}}</b>. <icon/> is an icon.";
 function convertMessageSyntax(msg) {
-    const replacedTags = msg
-        .replace(/<(\S+)>(.*)<\/\1>/g, "{+$1}$2{-$1}")  // Convert open/close function syntax
-        .replace(/<(\S+)\/>/g, "{#$1}")                 // Convert standalone function syntax
-        .replace(/{{(\S+)}}/g, "{$$$1}")                // Convert variable expression syntax
-    return `{${replacedTags}}`                          // Wrap message, TODO: fix after text mode change
+    var replacedTags = msg
+        .replace(/<(\S+)>(.*)<\/\1>/g, "{+$1}$2{-$1}") // Convert open/close function syntax
+        .replace(/<(\S+)\/>/g, "{:$1}") // Convert standalone function syntax
+        .replace(/{{(\S+)}}/g, "{$$$1}"); // Convert variable expression syntax
+    return "{".concat(replacedTags, "}"); // Wrap message, TODO: fix after text mode change
 }
-
-const convertedMsg = convertMessageSyntax(msg)
-
-// console.log(convertedMsg)
-
-const mf = new MessageFormat(convertedMsg, 'en');
-console.log(mf.resolveMessage({count: 42}).toString());
+var convertedMsg = convertMessageSyntax(msg);
+console.log(convertedMsg);
+// const functions = {
+//     link: ({ source, locales: [locale] }, _opt, input) => ({
+//       type: 'custom',
+//       source,
+//       locale,
+//       toParts: () => [
+//         { type: 'custom', source, locale, value: `part:${input}` }
+//       ],
+//       toString: () => "<link>heyo!</link>"
+//     })
+// };
+var mf = new messageformat_1.MessageFormat(convertedMsg, 'en', {
+    functions: {}
+});
+console.log(mf.format({ count: 42 }));
+// function newMessageValue(): MessageValue {
+//     return {
+//         locale: 'en',
+//         type: 'literal',
+//         source: 'NA',
+//         toString: () => 'hello there!'
+//     }
+// }
