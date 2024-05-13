bb06913b2a76db4ac3c7e01624af11ebcdd0080e
Tim Chevalier <tjc@igalia.com>
2024-05-13 12:49:22 -0700

    Move contents of mftest.tsx into index.tsx and edit README

--- demo/README.md
+++ demo/README.md
@@ -1,40 +1,29 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+This demo is a [Next.js](https://nextjs.org/) project. It aims to provide functionality akin to [next-translate](https://github.com/aralroca/next-translate) on top of the MessageFormat 2 JavaScript implementation.
 
 ## Getting Started
 
-First, run the development server:
+### Install dependencies
+
+```bash
+npm install
+```
+
+### Running the development server
 
 ```bash
 npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
+### Editing pages
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
+You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file. Start by reading the `export default function Demo()` at the end of the file.
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+### Message data
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+The demo reads messages in JSX format stored in JSON files. It converts the message to MF2 and uses the MF2 polyfill to format the result.
 
-## Learn More
+An example data file is stored under `locales/en-US/`. To try using different files, edit the `i18nkey` attribute in the `MF2Trans` component in `pages/index.tsx`. For example, it currently has the `i18nkey` set to "messages:default-message". This loads the file `locales/en-US/messages.json` and selects the `default-message` message defined in that file. Other directories for different locales could be added under `locales/`.
 
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+You can also embed a literal message in `pages/index.tsx` by removing the `i18nkey` attribute and adding a `message` attribute, which takes a literal string that's a message in JSX format.
--- demo/pages/index.tsx
+++ demo/pages/index.tsx
@@ -1,22 +1,204 @@
-import Trans from "next-translate/Trans"
+import React from "react";
+import { useEffect, useState, type Dispatch, type SetStateAction } from "react";
+import {
+	MessageFormat,
+	type MessageLiteralPart,
+	type MessageMarkupPart,
+	type MessagePart,
+} from "messageformat";
 
-// export default function Home() {
-//     return (
-//         <Trans
-//             i18nKey="justdemo"
-//             components={{
-//                 link: <a href="/" />,
-//                 b: <b style={{ color: "purple" }} />,
-//                 icon: <img src="https://imgs.xkcd.com/comics/purity.png"/>,
-//             }}
-//             // This is without pluralizing the count value
-//             values={{ count: 42 }}
-//             defaultTrans="Click <link>here</link>. {{count}} or <b>{{count}}</b>. <icon/> is an icon."
-//         />
-//     )
-// }
+function convertMessageSyntax(message: string) {
+	return message
+		.replace(/<(\w+\/?)>/g, "{#$1}") // open/standalone
+		.replace(/<(\/\w+)>/g, "{$1}") // close
+		.replace(/{{(\S+)}}/g, "{$$$1}"); // variable expressions
+}
 
-export default function Home() {
-//  const r = require('react');
-  return <Trans/>;
+type PartsListNode = MessagePart | Array<PartsListNode> | Markup;
+type PartsList = Array<PartsListNode>;
+class Markup {
+	#markup: boolean;
+	name: string;
+	child: PartsList;
+
+	constructor(name: string, child: PartsList) {
+		this.#markup = true;
+		this.name = name;
+		this.child = child;
+	}
+
+	static isMarkup(obj: object): boolean {
+		return #markup in obj;
+	}
+}
+
+function ProcessPartsList(parts: MessagePart[]): PartsList {
+	// Make a copy of `parts` so we can modify it
+	const toDo: MessagePart[] = [...parts];
+
+	// ProcessNodes() processes a flat list of message parts
+	// into a tree structure.
+	// (Currently only handles one level of nesting.)
+	// `accum` is the list of already-processed subtrees.
+	// The individual elements in the list are all `MessageParts`,
+	// but the lists in the returned value may be nested arbitrarily.
+	function ProcessNodes(accum: PartsList): PartsList {
+		if (toDo.length === 0) {
+			return accum;
+		}
+		// Markup node: should be an `open` node if the output of formatToParts()
+		// is valid.
+		if (toDo[0].type === "markup") {
+			const markupNode = toDo[0] as MessageMarkupPart;
+			if (markupNode.kind === "open") {
+				const openNode: MessageMarkupPart = toDo.shift() as MessageMarkupPart;
+				// Recursively process everything between the open and close nodes
+				const tree = ProcessNodes([]);
+				const closeNode: MessageMarkupPart = toDo.shift() as MessageMarkupPart;
+				if (closeNode.kind !== "close") {
+					console.log("Warning: unmatched tags!");
+				}
+				// Append a new subtree representing the tree denoted by this markup open/close pair
+				// TODO: To handle arbitrary nesting, we really want `tree` and not `...tree`
+				const subtree: Markup = new Markup(openNode.name, tree);
+				return ProcessNodes(accum.toSpliced(accum.length, 0, subtree));
+			}
+			// When we see a close tag, we just return the accumulator
+			if (markupNode.kind === "close") {
+				return accum;
+			}
+		}
+		// Default case (not markup): append onto the existing list
+		return ProcessNodes(
+			accum.toSpliced(accum.length, 0, toDo.shift() as MessagePart),
+		);
+	}
+	return ProcessNodes([]);
+}
+
+// hetList is really a list of arbitrarily-nested lists where all the
+// leaf elements are MessageParts
+function HetListToDOMTree(
+	hetList: PartsList,
+	components: Record<string, React.JSX.Element>,
+): JSX.Element[] {
+	return hetList.flatMap((part: PartsListNode) => {
+		// part is either a (nested) list of MessageParts, or a single MessagePart
+		if (Markup.isMarkup(part)) {
+			// `subtree` is all the nodes between the open and the close
+			const markup = part as Markup;
+			const subtree: JSX.Element[] = HetListToDOMTree(markup.child, components);
+			// Use the name of the open node to look up the component in the map
+			// (we assume open.name === close.name)
+			// TODO: this means overlapping tags don't work
+			const component = components[markup.name]; //assert
+			// Finally, wrap the sublist in a component of the kind
+			// that matches its markup's name
+			return React.cloneElement(component, undefined, ...subtree);
+		}
+		if (Array.isArray(part)) {
+			return HetListToDOMTree(part, components);
+		}
+		// If part is not an array, it must be a MessagePart
+		const messagePart = part as MessagePart;
+		switch (messagePart.type) {
+			case "literal":
+				// Literals are just strings
+				return <>{(messagePart as MessageLiteralPart).value}</>;
+			case "markup":
+				// assert part.kind=standalone
+				return React.cloneElement(
+					components[(messagePart as MessageMarkupPart).name],
+				);
+			case "number":
+			case "datetime": {
+				return (
+					<>{messagePart.parts?.reduce((acc, part) => acc + part.value, "")}</>
+				);
+			}
+			case "fallback": {
+				return <>{`{${messagePart.source}}`}</>;
+			}
+			default: {
+				throw new Error(`unreachable: ${messagePart.type}`);
+			}
+		}
+	});
+}
+
+
+type TransProps = {
+	i18nKey?: string;
+	locale: string;
+	message: string;
+	values: Record<string, unknown>;
+	components: Record<string, React.JSX.Element>;
+};
+
+// Splits a string like 'a:b' into {ns: a, suffix: b}
+function splitKey(k: string): { ns: string; suffix: string } {
+	const i = k.indexOf(":");
+	if (i < 0) throw new Error(`namespace required, got ${k}`);
+	return { ns: k.slice(0, i), suffix: k.slice(i + 1) };
+}
+
+// Not sure if this is necessary to do first:
+// npm i babel-plugin-syntax-dynamic-import -D
+// Takes a key like ns:name and looks for the ${ns}.json file
+// in the relevant locale directory. `name` is used to index
+// within the file.
+// TODO: should probably get this directory from config instead
+// of hardcoding it
+async function getMessage(i18nKey: string, lang: string): Promise<string> {
+	const { ns, suffix } = splitKey(i18nKey);
+	return await import(`../locales/${lang}/${ns}.json`).then(
+		(m) => m.default[suffix],
+	);
+}
+
+function MF2Trans(props: TransProps) {
+	const [element, setElement] = useState<JSX.Element[]>();
+	if (setElement) {
+		useEffect(mf2TransFun(props, setElement));
+		return <>{element}</>;
+	}
+	return <></>;
+}
+
+function mf2TransFun(
+	props: TransProps,
+	setElement: Dispatch<SetStateAction<JSX.Element[] | undefined>>,
+) {
+	return () => {
+		// If i18nKey is supplied, read a message from the given namespace.
+		// Otherwise, use our hardcoded message that gets converted to MF2.
+		const convertedPromise: Promise<string> = props.i18nKey
+			? getMessage(props.i18nKey, props.locale)
+			: new Promise(() => props.message);
+		convertedPromise.then((jsxMessage) => {
+      const converted = convertMessageSyntax(jsxMessage);
+			const mf = new MessageFormat(converted, props.locale);
+			const list = mf.formatToParts(props.values);
+			const processed = ProcessPartsList(list);
+			const contents = HetListToDOMTree(processed, props.components);
+			setElement(contents);
+		});
+	};
+}
+
+export default function Demo() {
+	return (
+		<MF2Trans
+			i18nKey="messages:default-message"
+			locale="en-US"
+			components={{
+				// biome-ignore lint/a11y/useAnchorContent: content will be added by the formatter
+				link: <a href="/" />,
+				b: <b style={{ color: "purple" }} />,
+				i: <i />,
+				icon: <img src="https://imgs.xkcd.com/comics/purity.png" alt="dummy" />,
+			}}
+			values={{ count: 42.2 }}
+		/>
+	);
 }
